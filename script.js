87f4bca77edab95aa488c89e47f9460c9845d9d9
Ryan Courtney <ryan.courtney1@gmail.com>
2016-11-29 21:49:51 -0500

    still trying to get buttons to work

--- script.js
+++ script.js
@@ -4,31 +4,42 @@
 // calling Jukebox.play() You need an array of song objects and the ability to play, 
 // stop and pause a song. How would you switch songs? Can you shuffle songs? Focus on getting 
 // the JavaScript to work and keep the HTML/CSS minimal, make it work before you make it look good!
+// Grading Criteria:
 
+// JS throws no errors
+// Pause, Play, and Stop functions work (and do not use built in functionality)
+// JukeBox is object-oriented
+// Page has at least one song on load and can play a different song based on title
+// Allow a user to load one of their own songs
+
+var playList = ["music/03 ProtoVision.m4a","music/08 Nightcall.m4a","music/indie.mp3","music/house.mp3"];
 var jukeBox = new Jukebox();
-var indie = new Song("Indie");
-var house = new Song("House");
-var protoVision = new Song("protoVision")
-var nightCall = new Song("nightCall")
+
 
 // var songList = [indie, house];
 
-jukeBox.addSong(indie)
-jukeBox.addSong(protoVision)
-jukeBox.addSong(house)
-jukeBox.addSong(nightCall)
+// jukeBox.addSong(music/03 ProtoVision.m4a);
+// jukeBox.addSong(music/08 Nightcall.m4a);
+// jukeBox.addSong(music/indie.mp3);
+// jukeBox.addSong(music/house.mp3);
 
 
 function Jukebox(){
-	this.addSong = function(song){
-		this.songList.push(song);
+		
+	this.play = function(){
+		document.getElementById("music").play();
 	}
-	this.songList = [];
-	this.play = function playAudio(){
-		document.getElementsbyClassName(playlist)
+	
+	this.pause = function(){
+		document.getElementById("music").pause();
+	}	
+	this.stop = function(){
+		document.getElementById("music").stop();
 	}
-}		
-
-function Song(name){
-	this.name = name;
 }
+
+var jukeBox = new Jukebox();
+
+document.getElementById("Play").addEventListener("click", Jukebox.play);
+document.getElementById("Pause").addEventListener("click", Jukebox.pause);
+document.getElementById("Stop").addEventListener("click", Jukebox.stop);
